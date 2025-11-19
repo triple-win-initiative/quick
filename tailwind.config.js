9fa988e15aab1b0f703a847dc2d2892d56329ae5
Volodymyr Sauliak <3429638@gmail.com>
2025-11-19 12:31:52 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './_layouts/**/*.{html,html}',
+    './_includes/**/*.{html,html}',
+    './_posts/**/*.{markdown,md}',
+    './assets/css/main.css', // Example path to your main CSS file
+  ],
+  plugins: [
+    require('daisyui'),
+  ],
+}
